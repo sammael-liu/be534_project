119693f545a2df22a462ef042754918c3068d95c
Wen-Wei Liu <wliu@Wen-Weis-MacBook-Air.local>
2021-11-25 07:14:18 +0800

    update

--- unit.py
+++
@@ -1,9 +0,0 @@
-from unittest import mock
-from unittest import TestCase
-from project import get_input
-
-def test_get_input():
-    """Test get_input"""
-
-    with mock.patch.object(__builtins__, 'input', lambda _: '123'):
-        assert get_input(4) == 'Error! "123" is not 4 digits long'
